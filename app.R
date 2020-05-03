e41cd6875c27ffe4ca48bd7ec497de06778d9560
ElisePupier <elise_pupier@hotmail.com>
2020-05-03 23:45:17 +0200

    modifs paramètres css

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+rm(list=ls()) #instruction permettant d'effacer la mémoire R
 
 library(jsonlite)
 library(plotly)
@@ -11,21 +11,24 @@ library(dashCoreComponents)
 library(dashTable)
 
 
+
 #---- PARAMETRES CSS ----
 
 type_font <- "barlow"
+size_title <- 24
+size_text <- 18
+size_text_axis <- 12
 
-col_titre <- "#454545"
-col_texte <- "#6A6A6A"
-
-col_no2 <- '#454545'
-col_o3 <- '#016FBF'
-col_pm25 <- '#F54A4A'
-col_pm10 <- '#FFA55E'
+col_title <- "#454545"
+col_text <- "#6A6A6A"
+col_graph <- "#1680A5"
+col_bleu_fonce <- "#194561"
+col_gris_leger <- "#F4F4F4"
 
 
 
 #---- IMPORT DATA ----
+
 path_particules <- "C:/Users/elise/Documents/Data for good/Particules/DataViz/Particules2/"
 
 df_list <- read_json(paste0(path_particules,"data_histo_noms_com_polluants2.json"))
@@ -34,7 +37,7 @@ df_list <- read_json(paste0(path_particules,"data_histo_noms_com_polluants2.json
 
 #---- DATA PREP ----
 
-#fonction pour enlever les accents
+#fonction pour enlever les accents sur value pour éviter les problèmes d'encodage
 Unaccent <- function(text) {
   text <- gsub("['`^~\"]", " ", text)
   text <- iconv(text, to="ASCII//TRANSLIT//IGNORE")
@@ -43,7 +46,9 @@ Unaccent <- function(text) {
 }
 
 
+
 #---- VALEURS UTILES ----
+
 #liste des communes 
 liste_communes <- names(df_list)
 
@@ -61,10 +66,10 @@ createHistogram <- function(input_commune, input_polluant){
   #les max et min serviront comme paramètres pour l'échelle des axes des absisses. Les valeurs à utiliser ont été déterminées à partir des données brutes
   #les seuils sont les valeurs recommandées par l'OMS
   input_polluant_list <- input_polluant #variable utilisée pour le calcul des PM2.5 et PM10 selon PM
-  if(input_polluant == "NO2"){col_pol <- col_no2 ; seuil_pol <- 40 ; min_pol <- 0 ; max_pol <- 80 
-  }else if(input_polluant == "O3"){col_pol <- col_o3 ; seuil_pol <- 100 ; min_pol <- 80 ; max_pol <- 160
-  }else if(input_polluant == "PM2.5"){col_pol <- col_pm25 ; seuil_pol <- 10 ; min_pol <- 0 ; max_pol <- 30 ; input_polluant_list <- "PM"
-  }else if(input_polluant == "PM10"){col_pol <- col_pm10 ; seuil_pol <- 20 ; min_pol <- 0 ; max_pol <- 70 ; input_polluant_list <- "PM"}
+  if(input_polluant == "NO2"){seuil_pol <- 40 ; min_pol <- 0 ; max_pol <- 80 
+  }else if(input_polluant == "O3"){seuil_pol <- 100 ; min_pol <- 80 ; max_pol <- 160
+  }else if(input_polluant == "PM2.5"){seuil_pol <- 10 ; min_pol <- 0 ; max_pol <- 30 ; input_polluant_list <- "PM"
+  }else if(input_polluant == "PM10"){seuil_pol <- 20 ; min_pol <- 0 ; max_pol <- 70 ; input_polluant_list <- "PM"}
   
   
   #--Dataframe à utiliser (variables : POPULATION et POLLUTION) pour cette commune et ce polluant
@@ -83,14 +88,14 @@ createHistogram <- function(input_commune, input_polluant){
   
   
   #--Graph 
-  fig <- plot_ly(type = "bar", name='') 
+  fig <- plot_ly(type = "bar", name = '', height = 200, width = 600) 
   
   #pour chaque polluant : ajout de l'histogramme, de la ligne rouge de seuil OMS, et du texte "Seuil de l'OMS"
   fig <- fig %>% 
     
     add_bars(data = data_com_pol,
              x = ~POLLUTION,  y = ~POPULATION,
-             name = input_polluant, marker=list(color=col_pol)) %>% 
+             name = input_polluant, marker=list(color=col_graph)) %>% 
     add_segments(x=seuil_pol,xend=seuil_pol, y=0,yend=1.2*(data_com_pol %>% summarise(max(POPULATION)))[[1]], 
                  line=list(color='red')) %>%
     add_text(x=seuil_pol*1.002,y=(data_com_pol %>% summarise(max(POPULATION)))[[1]],
@@ -99,14 +104,13 @@ createHistogram <- function(input_commune, input_polluant){
   #ajout des éléments de layout
   fig <- fig %>% layout(showlegend=FALSE,
                         title=input_polluant,
-                        xaxis=list(title="Niveau de pollution (µg/m3)",range = c(min_pol,max_pol)),
-                        yaxis=list(title="Population concernée"))
-  
+                        font = list(family = type_font, size = size_text_axis, color = col_text),
+                        xaxis=list(title = "Niveau de pollution (µg/m3)", range = c(min_pol,max_pol)),
+                        yaxis=list(title = "Nombre d'habitants"), 
+                        plot_bgcolor = col_gris_leger)
   
   fig
-
 }
-
 
 
 
@@ -121,84 +125,61 @@ if (appName != ""){
 }
 
 
-
 app <- Dash$new()
-
 
 
 app$layout(
   htmlDiv(
     list(
+      # Titre
       htmlDiv(
-        list(
-          htmlDiv(
-            list(
-              htmlH2(
-                "Population concernée par les différents niveaux de concentration en polluants",
-                id = "title",
-                style=list("margin-bottom"= "60px","margin-left"= "60px", "font-family"= type_font, 
-                           "font-size" = 18, color = col_titre)
-              )
-            )
-          )
-        ),
+        htmlH2(
+          "Habitants exposés à différents niveaux de pollution par commune",
+          id = "title",
+          style=list(marginTop = "40px", marginBottom = "30px", marginLeft = "160px", 
+                     fontFamily = type_font, fontSize = size_title, color = col_title)),
         className = "banner"
       ),
-      # Body
+      # Sélecteur de la commune
       htmlDiv(
         id = "controls-card",
         className = "container",
-        style = list(height = "10%", display = "flex", justifyContent = "space-around",
-                     paddingBottom = "3rem", width = "80%"),
+        style = list(fontFamily = type_font, fontSize = size_text, color = col_text,
+                     height = "10%", justifyContent = "right", display = "flex", 
+                     marginBottom = "40px", marginRight = "160px"),
         children = list(
           htmlDiv(
             id = "commune-select-outer",
-            style = list(width = "25%"),
+            style = list(width = "23%"),
             children = list(
-              htmlLabel("Sélectionnez une commune",style=list("font-family"= type_font, 
-                                                              "font-size" = 14, color = col_texte)),
+              htmlLabel("Sélectionnez une commune"),
               htmlDiv(
                 id = "commune-select-dropdown-outer",
-                children = list(
-                  dccDropdown(
-                    id = "commune-select",
-                    options = lapply(
-                      as.list(liste_communes),
-                      function(x){list(label = Unaccent(x), value = x)}
-                    ),
-                    value=input_commune,
-                    style=list("font-family"= type_font, "font-size" = 14, color = col_texte),
-                    searchable = TRUE #permet l'autocomplétion
-                  )
+                dccDropdown(
+                  id = "commune-select",
+                  options = lapply(
+                    as.list(liste_communes),
+                    function(x){list(label = x, value = Unaccent(x))}
+                  ),
+                  value=Unaccent(input_commune),
+                  style=list(backgroundColor = col_gris_leger),
+                  searchable = TRUE #permet l'autocomplétion
                 )
-              )
-            )
-          ),
-          
-          htmlDiv(
-            id = "polluant-select-outer",
-            children = list(
-              htmlLabel("Sélectionnez un polluant",style=list("font-family"= type_font, 
-                                                              "font-size" = 14, color = col_texte)),
-              dccDropdown(
-                id = "polluant-select",
-                options = list(list(label = 'NO2', value = 'NO2'),
-                               list(label = 'O3', value = 'O3'),
-                               list(label = 'PM2.5', value = 'PM2.5'),
-                               list(label = 'PM10', value = 'PM10')),
-                value="PM2.5",
-                style=list("font-family"= type_font, "font-size" = 14, color = col_texte)
               )
             )
           )
         )
       ),
+      
+      # Graphs
       htmlDiv(
-        list(
-          htmlDiv(
+        id = "graphs",
+        style = list(justifyContent = "space-between", boxSizing = "border-box"),
+        children = list(
+          htmlDiv( # Graphs du haut
             id = "top-graphs",
             style = list(display = "flex", justifyContent = "space-between", 
-                         width = "80%", margin = "0 auto"),
+                         width = "80%", height = "40vh", margin = "0 auto"),
             children = list(
               htmlDiv(
                 id = "left-top-graphs",
@@ -208,8 +189,7 @@ app$layout(
                     id = "id-histo-pm25",
                     figure = createHistogram(input_commune,"PM2.5")
                   )
-                ),
-                style = list(width = "100%", float = "center", boxSizing = "border-box")
+                )
               ),
               htmlDiv(
                 id = "right-top-graphs",
@@ -219,15 +199,14 @@ app$layout(
                     id = "id-histo-pm10",
                     figure = createHistogram(input_commune,"PM10")
                   )
-                ),
-                style = list(width = "100%", float = "center", boxSizing = "border-box")
+                )
               )
             )
           ),
-          htmlDiv(
+          htmlDiv( # Graphs du bas
             id = "bottom-graphs",
             style = list(display = "flex", justifyContent = "space-between", 
-                         width = "80%", margin = "0 auto"),
+                         width = "80%", height = "30vh", margin = "0 auto"),
             children = list(
               htmlDiv(
                 id = "left-bottom-graphs",
@@ -237,8 +216,7 @@ app$layout(
                     id = "id-histo-no2",
                     figure = createHistogram(input_commune,"NO2")
                   )
-                ),
-                style = list(width = "100%", float = "center", boxSizing = "border-box")
+                )
               ),
               htmlDiv(
                 id = "right-bottom-graphs",
@@ -248,8 +226,7 @@ app$layout(
                     id = "id-histo-o3",
                     figure = createHistogram(input_commune,"O3")
                   )
-                ),
-                style = list(width = "100%", float = "center", boxSizing = "border-box")
+                )
               )
             )
           )
@@ -260,43 +237,40 @@ app$layout(
 )
 
 
+# Réactivité avec les callbacks qui modifient les graphiques en fonction de la commune
 
 app$callback(
   output("id-histo-pm25", "figure"),
-  list(input("commune-select", "value"),
-       input("polluant-select", "value")),
+  list(input("commune-select", "value")),
   
-  function(input_commune,input_polluant){
+  function(input_commune){
     createHistogram(input_commune,"PM2.5")
   }
 )
 
 app$callback(
   output("id-histo-pm10", "figure"),
-  list(input("commune-select", "value"),
-       input("polluant-select", "value")),
+  list(input("commune-select", "value")),
   
-  function(input_commune,input_polluant){
+  function(input_commune){
     createHistogram(input_commune,"PM10")
   }
 )
 
 app$callback(
   output("id-histo-no2", "figure"),
-  list(input("commune-select", "value"),
-       input("polluant-select", "value")),
+  list(input("commune-select", "value")),
   
-  function(input_commune,input_polluant){
+  function(input_commune){
     createHistogram(input_commune,"NO2")
   }
 )
 
 app$callback(
   output("id-histo-o3", "figure"),
-  list(input("commune-select", "value"),
-       input("polluant-select", "value")),
+  list(input("commune-select", "value")),
   
-  function(input_commune,input_polluant){
+  function(input_commune){
     createHistogram(input_commune,"O3")
   }
 )
